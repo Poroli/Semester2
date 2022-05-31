7dafe7e284750d2bdc5a3417c53f16021569b5cf
N.O.W <Weicholdo02@gmail.com>
2022-05-31 15:58:00 +0200

    Slide/Dash wie auch immer

--- Assets/Scipts/PlattformerMovement.cs
+++ Assets/Scipts/PlattformerMovement.cs
@@ -6,19 +6,27 @@ public class PlattformerMovement : MonoBehaviour
 {
     public float MoveSpeed = 5f;
     public float JumpForce = 7f;
+    public float Slidestartspeed;
+    public float Slidetime;
+    public float ActualSpeed;
 
-    private Rigidbody2D rigidbody;
+    private Rigidbody2D rb2d;
     private Groundcheck groundCheck;
     private Animator playerAnimator;
     private SpriteRenderer playerRenderer;
+    private int canmove = 1;
+    private bool sliding = false;
 
 
     private void Start()
     {
-        rigidbody = GetComponent<Rigidbody2D>();
+        rb2d = GetComponent<Rigidbody2D>();
         groundCheck = GetComponent<Groundcheck>();
         playerAnimator = GetComponentInChildren<Animator>();
         playerRenderer = GetComponentInChildren<SpriteRenderer>();
+
+        Slidestartspeed = 0;
+        Slidetime = 1f;
     }
 
     private void Update()
@@ -27,8 +35,11 @@ public class PlattformerMovement : MonoBehaviour
         Vector2 newVelocity = new Vector2();
         float horizontalInput = Input.GetAxisRaw("Horizontal");
 
-        newVelocity.x = horizontalInput * MoveSpeed;
-        newVelocity.y = rigidbody.velocity.y;
+        if (canmove == 1)
+        {
+            newVelocity.x = horizontalInput * MoveSpeed;
+        }
+        newVelocity.y = rb2d.velocity.y;
 
         if (Input.GetKeyDown(KeyCode.Space) && groundCheck.CanJump())
         {
@@ -36,9 +47,37 @@ public class PlattformerMovement : MonoBehaviour
             groundCheck.Jumped();
             justJumped = true;
         }
-
-        rigidbody.velocity = newVelocity;
+        
+        rb2d.velocity = newVelocity;
         SetAnimations(newVelocity, justJumped);
+
+        Slidestartspeed = Input.GetAxisRaw("Horizontal") * MoveSpeed;
+        Vector2 Slide = new Vector2 ();
+
+        if (Input.GetKey(KeyCode.LeftShift))
+        {
+            canmove = 0;
+            
+            ActualSpeed = Slidestartspeed / (Slidetime * Slidetime);
+            if (Mathf.Abs(ActualSpeed) <= 1f )
+            {
+                ActualSpeed = 0;
+            }
+            Slide.y = rb2d.velocity.y;
+            Slide.x = ActualSpeed;
+            Slidetime += Time.deltaTime;
+            
+            rb2d.velocity = Slide;
+            sliding = true;
+
+
+        }
+        if (Input.GetKeyUp(KeyCode.LeftShift))
+        {
+            canmove = 1;
+            Slidetime = 1f;
+            sliding = false;
+        }
     }
 
     private void SetAnimations(Vector2 velocity, bool justJumped)
