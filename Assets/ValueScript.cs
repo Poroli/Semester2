b8214831dee11334e8be7295523c35f81ae9877e
N.O.W <Weicholdo02@gmail.com>
2022-06-28 11:43:16 +0200

    Souls

--- Assets/ValueScript.cs
+++ Assets/ValueScript.cs
@@ -11,4 +11,6 @@ public class ValueScript : ScriptableObject
     public float MaxSlideStack;
     public int AirslideAmmount;
     public float AttackCooldown;
+    public int Souls;
+    public int Soulsperkill;
 }
