b1b4484aaa4d7b914ead788741d725fed17c7bfd
N.O.W <Weicholdo02@gmail.com>
2022-06-28 11:05:57 +0200

    pralpha Prefabs

--- Assets/Scripts/Stats_Handler.cs
+++ Assets/Scripts/Stats_Handler.cs
@@ -53,7 +53,7 @@ public class Stats_Handler : MonoBehaviour
             CanGetSpeed = false;
         }
         SlideSpeed = Mathf.Abs(P_Movement.Slidestartspeed) + Slide_SpeedUpgrade + Slide_Xtrastartspeed;
-        Player_DMG = Weapon_DMG + (Weapon_DMG * PercentageofMaxSlided);
+        Player_DMG = Weapon_DMG + (Weapon_DMG * (PercentageofMaxSlided/100));
 
         PercentageofMaxSlided = Mathf.RoundToInt(((10 * P_Movement.Slidetime) / (10 * Slidetimereset)) * 100);
     }
